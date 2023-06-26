fec0ff5eeaaccf4e9c824e1618ba6c4322b7ae25
hjc <hjc_0827@163.com>
2023-06-26 23:39:22 +0800

    no Repeat Nums

---
+++ src/test/noRepeatNums.test.ts
@@ -0,0 +1,13 @@
+import { noRepeatNums } from "../noRepeatNums";
+
+describe("NoRepeatNums", () => {
+  test("Empty str", () => {
+    const result = noRepeatNums("");
+    expect(result).toBe(0);
+  });
+
+  test("Three str", () => {
+    const result = noRepeatNums("abcabcbb");
+    expect(result).toBe(3);
+  });
+});
